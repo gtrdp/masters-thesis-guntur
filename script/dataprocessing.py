20ec6dd6cc266f33dd402ed3d29440d5e03f3773
Guntur Dharma Putra <guntur.dharma@gmail.com>
2016-10-04 00:06:26 +0200

    working code to show the graph

--- script/dataprocessing.py
+++ script/dataprocessing.py
@@ -3,15 +3,18 @@
 # These following packages are required:
 # - scipy
 
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import re, os
+from collections import Counter
+from itertools import chain
+import pprint
 
 # plt.plot([1,2,3,4], [1,4,9,16])
 # plt.axis([0, 6, 0, 20])
 # plt.show()
 
-access_point = []
-probe_request = []
+access_point = dict()
+probe_request = dict()
 
 # the access point file
 # split the columns using regex
@@ -21,12 +24,14 @@ for filename in os.listdir("data/"):
     	(location, scan_type, scan_date, scan_time) = filename.split("-")
     	if scan_type == "ap" :
     		# access point
-			d = []
+			foo = []
+			scan_time = scan_time[:-4]
+
 			with open("data/" + filename) as f:
 			    for line in f:
 					ssid = ""
 					bssid = ""
-					rssi = ""
+					# rssi = ""
 					regex = r'(.*) ([a-z|0-9]{2}:[a-z|0-9]{2}:[a-z|0-9]{2}:[a-z|0-9]{2}:[a-z|0-9]{2}:[a-z|0-9]{2}) (-[0-9]* )'
 					
 					# get the bssid
@@ -38,17 +43,33 @@ for filename in os.listdir("data/"):
 					if matches:
 						ssid = matches.group(1).strip()
 					# get the signal strength
-					matches = re.match(regex, line)
-					if matches:
-						rssi = matches.group(3)
+					# matches = re.match(regex, line)
+					# if matches:
+					# 	rssi = matches.group(3)
 
 					# append that to the list
-					if bssid and ssid and rssi:
-						d.append({'bssid': bssid, 'ssid': ssid, 'rssi': rssi})
-					# write to file (if needed)
+					if bssid and ssid: #and rssi:
+						foo.append({'bssid': bssid, 'ssid': ssid}) #, 'rssi': rssi})
+			
+			if location not in access_point:
+				# new record
+				access_point[location] = {'total': foo, 'timely': [len(foo)]}
+			else:
+				access_point[location]['total'].extend(foo)
+				access_point[location]['timely'].append(len(foo))
 
-			print d
+			# print d
 
+# show up the result
+pp = pprint.PrettyPrinter(indent=4)
+for location in access_point:
+	print location
+	pp.pprint(access_point[location]['timely'])
+	
+	plt.plot(access_point[location]['timely'])
+	# plt.axis([0, 6, 0, 20])
+	
+plt.show()
 # the probe request file
 
 # from the dictionary, generate the graph
