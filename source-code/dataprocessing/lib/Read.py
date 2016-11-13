b286efdc710e245d45df386b4674b16dbc112bd3
Guntur Dharma Putra <guntur.dharma@gmail.com>
2016-11-14 00:22:20 +0100

    data processing with location labels

--- source-code/dataprocessing/lib/Read.py
+++ source-code/dataprocessing/lib/Read.py
@@ -5,11 +5,16 @@ import os, re
 import subprocess
 
 class Read:
+	# features variable
 	access_point = dict()
 	probe_request = dict()
 	audio_record = dict()
-	decibel_sound = dict()
+	rms = dict()
+	pklv = dict()
 	ground_truth = dict()
+	rssi = dict()
+	snr = dict()
+
 	oui_list = dict()
 
 	threshold = 0
@@ -50,14 +55,17 @@ class Read:
 					self.groundTruthChecking(filename, scan_time, location)
 
 		# return the result
-		return self.access_point, self.probe_request, self.audio_record, self.ground_truth, scan_date
+		return self.access_point, self.probe_request, self.audio_record, self.ground_truth, self.pklv, self.rms, self.rssi, self.snr, scan_date
 
 	def countProbe(self, filename, scan_time, location):
 		foo = dict()
 		scan_time = scan_time[:-4]
+		snr_sum = 0
+		snr_count = 0
 
 		with open("data/" + filename) as f:
 			for line in f:
+				# get the mac addresses
 				macaddr = ""
 				regex = r'SA:[a-z|0-9]{2}:[a-z|0-9]{2}:[a-z|0-9]{2}:[a-z|0-9]{2}:[a-z|0-9]{2}:[a-z|0-9]{2}'
 
@@ -88,6 +96,21 @@ class Read:
 					else:
 						foo[macaddr] = foo[macaddr] + 1
 
+				# get the snr
+				# the pattern:
+				# -79[dB signal] -96[dB noise]
+				# regex = r'SA:[a-z|0-9]{2}:[a-z|0-9]{2}:[a-z|0-9]{2}:[a-z|0-9]{2}:[a-z|0-9]{2}:[a-z|0-9]{2}'
+				regex = r'\-[0-9]+'
+				matches = re.findall(regex, line)
+
+				# get the snr
+				if matches and len(matches) > 1:
+					snr = int(matches[0]) - int(matches[1])
+					snr_sum += snr
+					snr_count += 1
+				else:
+					continue
+
 		# data removal
 		if self.threshold > 0:
 			# remove insignificant mac address
@@ -102,15 +125,24 @@ class Read:
 			self.probe_request[location]['total'] = foo
 			self.probe_request[location]['timely'].append(len(foo))
 
+		# count the average snr
+		foo = 0 if snr_count == 0 else snr_sum/snr_count
+		if location not in self.snr:
+			# new record
+			self.snr[location] = [foo]
+		else:
+			self.snr[location].append(foo)
+
 	def countAccessPoint(self, filename, scan_time, location):
 		ap_bssid = dict()
 		scan_time = scan_time[:-4]
+		rssi_sum = 0
+		rssi_count = 0
 
 		with open("data/" + filename) as f:
 			for line in f:
 				ssid = ""
 				bssid = ""
-				# rssi = ""
 				regex = r'(.*) ([a-z|0-9]{2}:[a-z|0-9]{2}:[a-z|0-9]{2}:[a-z|0-9]{2}:[a-z|0-9]{2}:[a-z|0-9]{2}) (-[0-9]* )'
 
 				# get the bssid
@@ -121,17 +153,28 @@ class Read:
 				matches = re.match(regex, line)
 				if matches:
 					ssid = matches.group(1).strip()
-				# get the signal strength
-				# matches = re.match(regex, line)
-				# if matches:
-				# 	rssi = matches.group(3)
 
 				# append that to the list
 				# beware of doubled mac addresses
-				if bssid and ssid:  # and rssi:
+				if bssid and ssid:
 					# check if already in a list
 					if bssid not in ap_bssid:
 						ap_bssid[bssid] = ssid
+
+				# get the rssi
+				# the pattern:
+				# [:e8 ]-85[  136]
+				regex = r':[a-z|0-9]{2}\s\-[0-9]+\s'
+				matches = re.findall(regex, line)
+
+				# get the snr
+				if matches:
+					current_rssi = int(matches[0][4:])
+
+					rssi_sum += current_rssi
+					rssi_count += 1
+				else:
+					continue
 
 		if location not in self.access_point:
 			# new record
@@ -139,6 +182,13 @@ class Read:
 		else:
 			self.access_point[location]['total'].update(ap_bssid)
 			self.access_point[location]['timely'].append(len(ap_bssid))
+
+		# count the average snr
+		if location not in self.rssi:
+			# new record
+			self.rssi[location] = [rssi_sum/rssi_count]
+		else:
+			self.rssi[location].append(rssi_sum/rssi_count)
 
 	def countVoice(self, filename, scan_time, location):
 		# run the jar file
@@ -157,6 +207,42 @@ class Read:
 			self.audio_record[location]['total'] += foo_int
 			self.audio_record[location]['timely'].append(foo_int)
 			# end if
+
+		# check the SNR
+		regex_peak = r'Pk lev dB\s+\S+'
+		regex_rms = r'RMS lev dB\s+\S+'
+		foo = subprocess.Popen(['sox', 'data/' + filename, '-n', 'stats'], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+						 stdin=subprocess.PIPE).communicate()[1]
+		foo = foo.strip()
+
+		peak = re.findall(regex_peak, foo)
+		rms = re.findall(regex_rms, foo)
+
+		# get the peak level
+		if peak:
+			peak = float(peak[0].replace('Pk lev dB', '').strip())
+		else:
+			print "peak not found"
+			peak = 0
+
+		if location not in self.pklv:
+			# new record
+			self.pklv[location] = [peak]
+		else:
+			self.pklv[location].append(peak)
+
+		# get the rms level
+		if rms:
+			rms = float(rms[0].replace('RMS lev dB', '').strip())
+		else:
+			print "rms not found"
+			rms = 0
+
+		if location not in self.rms:
+			# new record
+			self.rms[location] = [rms]
+		else:
+			self.rms[location].append(rms)
 
 	def groundTruthChecking(self, filename, scan_time, location):
 		self.ground_truth[location] = list()
