576dcb2b28b4fbd26dd733c2494c6f6c5c5c8830
Guntur Dharma Putra <guntur.dharma@gmail.com>
2017-01-18 03:59:59 +0100

    first final draft

--- source-code/dataprocessing/readsnr.py
+++ source-code/dataprocessing/readsnr.py
@@ -2,7 +2,6 @@
 from lib.SNR import SNR
 from lib.AmbientSound import AmbientSound
 
-<<<<<<< HEAD
 # snrReader = SNR()
 #
 # snrReader.readProbeSNR()
@@ -10,12 +9,3 @@ from lib.AmbientSound import AmbientSound
 
 ambient = AmbientSound()
 ambient.readAmbientSound()
-=======
-snrReader = SNR()
-
-snrReader.readProbeSNR()
-snrReader.readAPSignal()
-
-# ambient = AmbientSound()
-# ambient.readAmbientSound()
->>>>>>> deb4eee798046ff3050e2fdc49aff179daa28237
